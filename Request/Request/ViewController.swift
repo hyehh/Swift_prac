6ecc3fd444f8339f0137979d771adf74d75a1796
hyehh <hyeji1203@icloud.com>
2021-09-29 12:03:24 +0900

    2021.09.29 First Commit

---
+++ Request/Request/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Request
+//
+//  Created by HYEJI on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let url = "https://api.sunrise-sunset.org/json?date=2020-01-01&lat=-74.0060&lng=40.7128&formatted=0"
+        getData(from: url)
+    }
+
+    private func getData(from url:String) {
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+            
+            // have data
+            var result: Response?
+            do{
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }catch {
+                print("failed to convert \(error.localizedDescription)")
+            }
+            guard let json = result else {
+                return
+            }
+            
+            print(json.status)
+            print(json.results.sunrise)
+            print(json.results.sunset)
+            print(json.results.solar_noon)
+        })
+            task.resume()
+    }
+}
+
+
+struct Response: Codable {
+    let results: MyResult
+    let status: String
+}
+ 
+struct MyResult: Codable {
+    let sunrise: String
+    let sunset: String
+    let solar_noon: String
+    let day_length: Int
+    let civil_twilight_begin: String
+    let civil_twilight_end: String
+    let nautical_twilight_begin: String
+    let nautical_twilight_end: String
+    let astronomical_twilight_begin: String
+    let astronomical_twilight_end: String
+}
+
+//{"results":{"sunrise":"1970-01-01T00:00:01+00:00","sunset":"1970-01-01T00:00:01+00:00","solar_noon":"2020-01-01T09:20:25+00:00","day_length":0,"civil_twilight_begin":"1970-01-01T00:00:01+00:00","civil_twilight_end":"1970-01-01T00:00:01+00:00","nautical_twilight_begin":"1970-01-01T00:00:01+00:00","nautical_twilight_end":"1970-01-01T00:00:01+00:00","astronomical_twilight_begin":"1970-01-01T00:00:01+00:00","astronomical_twilight_end":"1970-01-01T00:00:01+00:00"},"status":"OK"}
