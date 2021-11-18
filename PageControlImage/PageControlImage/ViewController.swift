8fac42c633d949e39b3f815e9009bca02c9f7a2e
hyehh <hyeji1203@icloud.com>
2021-11-18 18:07:52 +0900

    2021.11.18 Hyeji First Commit

---
+++ PageControlImage/PageControlImage/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PageControlImage
+//
+//  Created by HYEJI on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let pc = LKPageControl.init(frame: .zero)
+        pc.currentPageIndicatorTintColor = .green
+        pc.pageIndicatorTintColor = .blue
+        pc.numberOfPages = 2
+        pc.currentPage = 0
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        pc.addTarget(self, action: #selector(onPageChanged(_:)), for: .valueChanged)
+        view.addSubview(pc)
+        
+        self.view.addConstraints([
+            
+            pc.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            pc.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            pc.heightAnchor.constraint(equalToConstant: 35),
+            pc.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5, constant: 0)
+            
+        ])
+    }
+
+    @objc func onPageChanged(_ sender: LKPageControl) {
+      print("page \(sender.currentPage) is selected")
+    }
+    
+}
+
